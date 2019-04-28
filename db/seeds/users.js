744863a690cd19ae8bf4e661caec7aa6815c7548
Melanie Burger <melanie.burger@rangle.io>
2019-04-28 15:51:23 -0400

    🤡 Mocks users

---
+++ db/seeds/users.js
@@ -0,0 +1,66 @@
+const faker = require("faker");
+
+const genders = ["Woman", "Agender", "Non-binary", "Man"];
+
+const pronouns = ["She/her", "They/their", "Him/his"];
+
+const identities = [
+  "Person of colour",
+  "Indigenous person",
+  "LGBTQIA+",
+  "Person with a disability",
+  "Prefer not to disclose"
+];
+
+const createUser = () => ({
+  id: faker.random.uuid(),
+  first_name: faker.name.firstName(),
+  last_name: faker.name.lastName(),
+  email: faker.internet.email(),
+  role: "student",
+  employment_status: faker.random.arrayElement([
+    "full_time",
+    "part_time",
+    "in_school",
+    "looking",
+    "not_looking"
+  ]),
+  employer: faker.company.companyName(),
+  genders_id: faker.random.number(genders.length - 1),
+  pronouns_id: faker.random.number(pronouns.length - 1),
+  identities_id: faker.random.number(identities.length - 1)
+});
+
+const createGenders = () =>
+  genders.map((gender, i) => ({
+    id: i,
+    gender
+  }));
+
+const createPronouns = () =>
+  pronouns.map((pronoun, i) => ({
+    id: i,
+    pronoun
+  }));
+
+const createIdentities = () =>
+  identities.map((identity, i) => ({
+    id: i,
+    identity
+  }));
+
+const createNUsers = n => Array.from(Array(n), createUser);
+
+exports.seed = (knex, Promise) => {
+  const emptyTables = Promise.all([
+    knex("users").del(),
+    knex("identities").del(),
+    knex("pronouns").del(),
+    knex("genders").del()
+  ]);
+  return emptyTables
+    .then(() => knex("genders").insert(createGenders()))
+    .then(() => knex("pronouns").insert(createPronouns()))
+    .then(() => knex("identities").insert(createIdentities()))
+    .then(() => knex("users").insert(createNUsers(20)));
+};

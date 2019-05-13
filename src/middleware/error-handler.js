3108f6a1f9d2bef2696df1ae897076a70d99dd25
Melanie Burger <melanie.burger@rangle.io>
2019-05-13 17:24:20 -0400

    🐛 Fixes error logger

--- src/middleware/error-handler.js
+++ src/middleware/error-handler.js
@@ -1,16 +1,16 @@
-const logger = require('../utils/logger');
+const logger = require("../utils/logger").logger;
 
-const errorHandler = (
-  err,
-  req,
-  res,
-  next
-) => {
+const errorHandler = (err, req, res, next) => {
   logger.error(err.message);
+  if (process.env.NODE_ENV !== "production") {
+    logger.error(err.stack);
+  }
   if (!err.statusCode) err.statusCode = 500;
-  res.status(err.statusCode).send(err.message);
+  res.status(err.statusCode).json({
+    error: err.message
+  });
 };
 
 module.exports = {
   errorHandler
-}
+};

7e7b00df3abe70041963fa1b78abd60f76a3386a
Melanie Burger <melanie.burger@rangle.io>
2019-05-13 17:24:01 -0400

    🔧 Updates configuration files

--- src/index.js
+++ src/index.js
@@ -5,10 +5,7 @@ const router = require("./api");
 const { logger } = require("./utils/logger");
 const { errorHandler } = require("./middleware/error-handler");
 
-// Create a new express application instance
 const app = express();
-
-// The port the express app will listen on
 const port = 3000;
 
 logger.info("🤖 Initializing middleware");
@@ -17,7 +14,6 @@ app.use(morgan("tiny", { stream: logger.stream }));
 app.use("/", router);
 app.use(errorHandler);
 
-// Serve the application at the given port
 app.listen(port, () => {
   logger.info(`🎧 Listening at http://localhost:${port}/`);
 });

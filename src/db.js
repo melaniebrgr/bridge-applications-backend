0670195661bfd5d40a88164e84d7e37a2c6f989a
Melanie Burger <melanie.burger@rangle.io>
2019-05-14 07:09:42 -0400

    🔧 Adds single db config

--- src/db.js
+++ src/db.js
@@ -4,8 +4,6 @@ const configuration = require("../knexfile");
 
 const KnexInstance = Knex(configuration);
 
-//Init Model with Knex only once in app
 Model.knex(KnexInstance);
 
-//Init the Knex instance only once, as it create a connection pool.
 module.exports = KnexInstance;

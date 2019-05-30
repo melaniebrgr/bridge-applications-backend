4c096e90a6ab394dc14f42db284970341e293ba6
Melanie Burger <melanie.burger@rangle.io>
2019-05-29 20:31:52 -0400

    Updates knexfile

--- knexfile.js
+++ knexfile.js
@@ -1,10 +1,20 @@
 module.exports = {
   client: "pg",
   connection: {
-    host: "db", // comment this out to run migrations
-    user: "admin",
-    password: "admin",
-    database: "bridge-applications-local"
+    database:
+      process.env.NODE_ENV === "development"
+        ? "bridge-applications-local"
+        : process.env.RDS_DB_NAME,
+    host:
+      process.env.NODE_ENV === "development" ? "db" : process.env.RDS_HOSTNAME,
+    user:
+      process.env.NODE_ENV === "development"
+        ? "admin"
+        : process.env.RDS_USERNAME,
+    password:
+      process.env.NODE_ENV === "development"
+        ? "admin"
+        : process.env.RDS_PASSWORD
   },
   migrations: {
     directory: `${__dirname}/db/migrations`
